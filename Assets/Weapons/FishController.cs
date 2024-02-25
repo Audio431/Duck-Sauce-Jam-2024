ba96c1eb5c226427ef6d3b6f46f338774016e55b
Atip Kajitamkul <au.atip@outlook.com>
2024-02-25 14:01:45 +0000

    Revert "Merge branch 'main' of https://github.com/Audio431/Duck-Sauce-Jam-2024"
    This reverts commit d9a48491384bd20314b10d953fa180e102774809, reversing
    changes made to d9a4e1ee246e57b7301cab5ed3ffb48fddc0095f.

--- Assets/Weapons/FishController.cs
+++ Assets/Weapons/FishController.cs
@@ -4,13 +4,10 @@ using UnityEngine;
 
 public class FishController : WeaponController
 {
-    [SerializeField] private AudioClip attackClip;
-
     // Start is called before the first frame update
     protected override void Attack(){
         base.Attack();
         GameObject spawnedFish = Instantiate(prefab);
-        SoundFXManager.instance.PlaySoundFXClip(attackClip, transform, 0.2f);
         spawnedFish.transform.position = transform.position;
         spawnedFish.transform.position = spawnedFish.transform.position + new Vector3(0.5f,0.5f);
         spawnedFish.GetComponent<FishBehaviour>().DirectChecker(pc.LastmoveVector);
